5b54f559067c96a1b7112159fa85d7b317bb78c7
neojarvis <techadmin@iamneo.ai>
2024-02-10 16:33:20 +0000

    first commit

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using CMSModel;
+namespace CMSApplication
+{
+
+
+  public class Program
+  {
+    static List<Student> studlist = new List<Student>();
+
+    
+    public   static void Main(string[] args)
+    {
+
+    
+
+      studlist.Add(new Student { Id = 101, Name = "Kirankumarbhai"});
+      studlist.Add(new Student { Id = 102, Name = "Sureshraine" });
+       studlist.Add(new Student { Id = 103, Name = "mahesh" });
+      
+      
+      foreach (Student st in studlist)
+      {
+        Console.WriteLine("{0}--{1}", st.Id, st.Name);
+      }
+
+
+    }
+
+
+
+  }
+}
+
+
