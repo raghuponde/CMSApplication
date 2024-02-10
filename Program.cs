598fe51d479c745e2f8aa3aabf372c8c4f8b9b83
neojarvis <techadmin@iamneo.ai>
2024-02-10 19:36:32 +0000

    ssadded

--- Program.cs
+++ Program.cs
@@ -14,7 +14,7 @@ namespace CMSApplication
     {
 
     
-    Console.WriteLine("Hellow ");
+    Console.WriteLine("Hellowssss ");
       studlist.Add(new Student { Id = 101, Name = "Kirankumarbhai"});
      
        studlist.Add(new Student { Id = 103, Name = "mahesh raine" });
